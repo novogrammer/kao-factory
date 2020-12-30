1b4960b3cde2778cda792e63fabfe86d13105153
Yusuke Kawamoto <novogrammer@gmail.com>
2020-12-31 06:23:58 +0900

    useContextに置き換えて簡略化、描画時に副作用があると良くないらしいのでuseEffectも使う。

--- client/components/Factory.js
+++ client/components/Factory.js
@@ -1,17 +1,12 @@
+import React from "react";
 import CameraContext from "../context/CameraContext";
 
 //パラメーターを送るだけ。
 export default function Factory(props){
   const {position}=props;
-  return (
-    <CameraContext.Consumer>
-      {
-        ({updatePosition})=>{
-          updatePosition(position);
-          return (<div>Factory:{JSON.stringify(position)}</div>);
-        }
-      }
-    </CameraContext.Consumer>
-
-  );
+  const {updatePosition}=React.useContext(CameraContext);
+  React.useEffect(()=>{
+    updatePosition(position);
+  });
+  return (<div>Factory:{JSON.stringify(position)}</div>);
 }
