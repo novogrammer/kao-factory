909db9ec351df2aaeebb7c0ab00c6d09f3ad3449
Yusuke Kawamoto <novogrammer@gmail.com>
2021-01-02 02:28:55 +0900

    tensorflow.jsのプロトタイプから移植

--- client/libs/EntryClientApp.js
+++ client/libs/EntryClientApp.js
@@ -1,6 +1,32 @@
-import { FPS_ENTRY } from "../../common/constants";
+import {
+  FPS_ENTRY,
+  VIDEO_SIZE,
+} from "../../common/constants";
 
 import ClientAppBase from "./ClientAppBase";
+
+import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import * as tf from '@tensorflow/tfjs-core';
+
+import * as tfjsWebgl from '@tensorflow/tfjs-backend-webgl';
+
+
+
+import { TRIANGULATION } from "./triangulation";
+
+function drawPath(ctx, points, closePath) {
+  const region = new Path2D();
+  region.moveTo(points[0][0], points[0][1]);
+  for (let i = 1; i < points.length; i++) {
+    const point = points[i];
+    region.lineTo(point[0], point[1]);
+  }
+
+  if (closePath) {
+    region.closePath();
+  }
+  ctx.stroke(region);
+}
 
 export default class EntryClientApp extends ClientAppBase {
   constructor(params) {
@@ -21,6 +47,8 @@ export default class EntryClientApp extends ClientAppBase {
       video,
       view,
     });
+    await this.setupModelAsync();
+    await this.setupCameraAsync();
 
     //onTickなどもあるので最後にする。
     //問題が起きれば実行順を数値で表すなどする。
@@ -33,6 +61,8 @@ export default class EntryClientApp extends ClientAppBase {
     //setupが終わってからdestroy
     await this.setupPromise;
     await super.destroyAsync();
+    await this.destroyCameraAsync();
+    await this.destroyModelAsync();
 
   }
   /**
@@ -48,5 +78,97 @@ export default class EntryClientApp extends ClientAppBase {
   destorySocketIo() {
     const { socket } = this;
     super.destorySocketIo();
+  }
+  async setupCameraAsync() {
+    const { video, view } = this;
+    const stream = await navigator.mediaDevices.getUserMedia({
+      'audio': false,
+      'video': {
+        facingMode: 'user',
+        width: VIDEO_SIZE,
+        height: VIDEO_SIZE
+      },
+    });
+    video.srcObject = stream;
+
+    await new Promise((resolve) => {
+      video.onloadedmetadata = () => {
+        resolve();
+      };
+    });
+    video.play();
+    view.width = VIDEO_SIZE;
+    view.height = VIDEO_SIZE;
+    const ctx = view.getContext("2d");
+    ctx.translate(view.width, 0);
+    ctx.scale(-1, 1);
+
+    Object.assign(this, {
+      stream,
+      ctx,
+    });
+  }
+  async destroyCameraAsync() {
+    const { stream } = this;
+
+  }
+  async setupModelAsync() {
+    const model = await faceLandmarksDetection.load(
+      faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
+    );
+    Object.assign(this, {
+      model,
+    });
+
+  }
+  async destroyModelAsync() {
+    //DO NOTHING
+  }
+  async onTickAsync() {
+    await super.onTickAsync();
+    const {
+      model,
+      video,
+    } = this;
+
+    const predictions = await model.estimateFaces({
+      input: video,
+    });
+
+    this.drawFaces(predictions);
+  }
+  drawFaces(predictions) {
+    const {
+      ctx,
+      view,
+      video,
+    } = this;
+    ctx.drawImage(
+      video,
+      0, 0, VIDEO_SIZE, VIDEO_SIZE,
+      0, 0, view.width, view.height
+    );
+    for (let i = 0; i < predictions.length; i++) {
+      const scaledKeypoints = predictions[i].scaledMesh;
+      // const keypoints = predictions[i].mesh;
+
+      ctx.save();
+      ctx.fillStyle = '#32EEDB';
+      ctx.strokeStyle = '#32EEDB';
+      ctx.lineWidth = 0.5;
+      for (let j = 0; j < TRIANGULATION.length; j += 3) {
+        const points = [
+          TRIANGULATION[j + 0],
+          TRIANGULATION[j + 1],
+          TRIANGULATION[j + 2],
+        ].map(index => scaledKeypoints[index]);
+
+        drawPath(ctx, points, true);
+      }
+      ctx.restore();
+    }
+
+
+
   }
 }
--- common/constants.js
+++ common/constants.js
@@ -5,3 +5,7 @@ export const ROOM_FACTORY = "room factory";
 export const FPS_ENTRY = 10;
 export const FPS_FACTORY = 60;
 
+export const VIDEO_SIZE = 512;
+// export const VIDEO_SIZE = 256;
+
+//const FOVY = 60;
